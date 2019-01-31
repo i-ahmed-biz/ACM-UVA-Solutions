12f779c7144f1fc6d22d7c27520d6050cf3ae4ce
Ahmed Inam <iahmed@gulfnet.co.jp>
2019-01-31 15:49:49 +0900

    ACM 11547 solution

---
+++ 11547/11547 - Automatic Answer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int t;
+    int x;
+    long int result;
+
+    cin>>t;
+
+    while(t--) {
+    	cin>>x;
+	    result = (((x * 315) + 36962) / 10) % 10;
+	    result = result < 0 ? -result: result;
+	    cout<<result<<endl;
+    }
+
+    return 0;
+}
