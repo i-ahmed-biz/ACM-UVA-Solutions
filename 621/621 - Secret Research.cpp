6a96ee4338bb47b9886cf789b1b48bed1b5c4b7d
Ahmed Inam <iahmed@gulfnet.co.jp>
2019-02-07 09:22:14 +0900

    ACM 621 solution

---
+++ 621/621 - Secret Research.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int tc;
+	string s;
+
+	cin>>tc;
+
+	while (tc--) {
+		cin>>s;
+
+		int len = s.length();
+
+		if (s == "0" || s == "4" || s == "78") {
+		    cout<<"+"<<endl;
+		}
+
+		else if (s.c_str()[len - 1] == '5' && s.c_str()[len - 2] == '3') {
+		    cout<<"-"<<endl;
+		}
+		else if (s.c_str()[0] == '9' && s.c_str()[len - 1] == '4') {
+		    cout<<"*"<<endl;
+		}
+		else if (s.c_str()[0] == '1' && s.c_str()[1] == '9' && s.c_str()[2] == '0') {
+		    cout<<"?"<<endl;
+		}
+		else {
+		    cout<<"+"<<endl;
+		}
+	}
+
+	return 0;
+}
